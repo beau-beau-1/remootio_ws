064b3d1f3497110d6cbc8d35052bbdb765f90e6e
beau-beau-1 <beau.raymond@outlook.com>
2026-02-21 17:51:54 +0800

    Update config_flow.py

--- custom_components/remootio_ws/config_flow.py
+++ custom_components/remootio_ws/config_flow.py
@@ -1,18 +1,19 @@
-import voluptuous as vol
 from homeassistant import config_entries
-from .const import DOMAIN, CONF_HOST, CONF_PORT, CONF_API_KEY, CONF_API_SECRET, DEFAULT_PORT
+from .const import DOMAIN, CONF_API_KEY, CONF_API_SECRET
 
 class RemootioConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    VERSION = 1
 
     async def async_step_user(self, user_input=None):
+        errors = {}
         if user_input is not None:
+            # Validate input here if needed
             return self.async_create_entry(title="Remootio 3", data=user_input)
 
-        schema = vol.Schema({
-            vol.Required(CONF_HOST): str,
-            vol.Optional(CONF_PORT, default=DEFAULT_PORT): int,
-            vol.Required(CONF_API_KEY): str,
-            vol.Required(CONF_API_SECRET): str,
-        })
-
-        return self.async_show_form(step_id="user", data_schema=schema)
+        data_schema = {
+            CONF_API_KEY: str,
+            CONF_API_SECRET: str,
+            "host": str,
+            "port": int,
+        }
+        return self.async_show_form(step_id="user", data_schema=data_schema, errors=errors)
