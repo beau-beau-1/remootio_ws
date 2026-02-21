d4d406b4d7920b8dcd52dce6d16c9080a4d9c344
beau-beau-1 <beau.raymond@outlook.com>
2026-02-21 17:52:30 +0800

    Update coordinator.py

--- custom_components/remootio_ws/coordinator.py
+++ custom_components/remootio_ws/coordinator.py
@@ -1,29 +1,19 @@
-from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
+import asyncio
 from homeassistant.core import HomeAssistant
-from datetime import timedelta
-import logging
 from .remootio_client import RemootioClient
 
-_LOGGER = logging.getLogger(__name__)
-
-class RemootioCoordinator(DataUpdateCoordinator):
-
+class RemootioCoordinator:
     def __init__(self, hass: HomeAssistant, entry):
-        self._entry = entry
+        self.hass = hass
+        self.entry = entry
         self.client = RemootioClient(
-            entry.data["host"],
-            entry.data["port"],
-            entry.data["api_key"],
-            entry.data["api_secret"],
-            self._state_updated,
+            host=entry.data["host"],
+            port=entry.data["port"],
+            api_key=entry.data["api_key"],
+            api_secret=entry.data["api_secret"],
+            state_callback=self._state_update,
         )
-
-        super().__init__(
-            hass,
-            _LOGGER,
-            name="Remootio",
-            update_interval=timedelta(seconds=30),
-        )
+        self._listeners = []
 
     async def async_setup(self):
         await self.client.connect()
@@ -31,8 +21,9 @@ class RemootioCoordinator(DataUpdateCoordinator):
     async def async_shutdown(self):
         await self.client.disconnect()
 
-    async def _async_update_data(self):
-        return self.client.state
+    def _state_update(self, state):
+        for listener in self._listeners:
+            listener(state)
 
-    def _state_updated(self, state):
-        self.async_set_updated_data(state)
+    def add_listener(self, callback):
+        self._listeners.append(callback)
