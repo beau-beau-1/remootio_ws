9cc1188093d50e013bd99aa7a68120844444a0c1
beau-beau-1 <beau.raymond@outlook.com>
2026-02-21 16:17:33 +0800

    Update __init__.py

--- custom_components/remootio_ws/__init__.py
+++ custom_components/remootio_ws/__init__.py
@@ -1,1 +1,23 @@
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+from .const import DOMAIN
+from .coordinator import RemootioCoordinator
 
+PLATFORMS = ["cover"]
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+    coordinator = RemootioCoordinator(hass, entry)
+    await coordinator.async_setup()
+
+    hass.data.setdefault(DOMAIN, {})
+    hass.data[DOMAIN][entry.entry_id] = coordinator
+
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+    return True
+
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
+    coordinator = hass.data[DOMAIN][entry.entry_id]
+    await coordinator.async_shutdown()
+    await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
+    return True
